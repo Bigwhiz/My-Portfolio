62fe220d4d2642214c24d0be03f589d697b9d645
Bigwhiz <wisdomwhizi@gmail.com>
2025-07-25 10:25:46 +0100

    Fresh start – my personal portfolio

---
+++ app/layouts/navbar/nav-data.js
@@ -0,0 +1,35 @@
+import config from '~/config.json';
+
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'Details',
+    pathname: '/#details',
+  },
+
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'X',
+    url: 'https://x.com/BigWhiz_001', // ← Change to your actual X handle
+    icon: 'x',
+  },
+  {
+    label: 'GitHub',
+    url: 'https://github.com/Bigwhiz',
+    icon: 'github',
+  },
+  {
+    label: 'Email',
+    url: 'mailto:wisdomwhizi@gmail.com', // ← update this to your actual Gmail
+    icon: 'mail',
+  },
+];
