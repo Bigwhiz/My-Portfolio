e8e8e1e07101cab07d119986ea1d9d1fe852d777
Bigwhiz <wisdomwhizi@gmail.com>
2025-07-27 17:03:10 +0100

    Fix Cloudflare: add adapter + build output

---
+++ build/client/assets/config-DPRwYq_l.js
@@ -0,0 +1,5 @@
+const i="Wisdom Osas",s="FullStack",o=["Frontend","Backend","UI/UX","JavaScript","Python"],n="",c="https://x.com/BigWhiz_001",t="https://github.com/Bigwhiz",a="mailwisdomosas@gmail.com",l="",e=`__  __  __
+\\ \\ \\ \\ \\∕
+ \\ \\∕\\ \\
+  \\∕  \\∕
+`,m={name:i,role:s,disciplines:o,url:n,x:c,github:t,gmail:a,repo:l,ascii:e};export{m as c};
